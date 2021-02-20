e7ac83fac5682e27eeaced53a2eb10f53bfab62b
Ricky Shah <ricky@Rickys-MacBook-Pro.local>
2021-02-19 21:41:46 -0500

    working

--- server.js
+++ server.js
@@ -17,6 +17,15 @@ app.use(
 );
 app.use(bodyParser.json());
 
+if (process.env.NODE_ENV === "production") {
+app.use(express.static("client/build"));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+})
+
+}
+
 // DB Config
 const db = require("./config/keys").mongoURI;
 
