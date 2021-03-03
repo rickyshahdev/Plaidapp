bb933ff636d0728ebd2c0e0cf8541f767b374fd3
Ricky Shah <ricky@Rickys-MacBook-Pro.local>
2021-03-03 18:39:46 -0500

    pushing

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ require('dotenv').config()
 const app = express();
 
 // Bodyparser middleware
+
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static("client/build"));
+
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  })
+}
 app.use(
   bodyParser.urlencoded({
     extended: false
