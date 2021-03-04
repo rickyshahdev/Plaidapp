7e63862fa561276ac8b843ad0da360a8bf2ec136
Ricky Shah <ricky@Rickys-MacBook-Pro.local>
2021-03-04 12:50:37 -0500

    branch

--- server.js
+++ server.js
@@ -11,13 +11,6 @@ const app = express();
 
 // Bodyparser middleware
 
-if (process.env.NODE_ENV === "production"){
-  app.use(express.static("client/build"));
-
-  app.get("*", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  })
-}
 app.use(
   bodyParser.urlencoded({
     extended: false
@@ -53,6 +46,6 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/plaid", plaid);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
